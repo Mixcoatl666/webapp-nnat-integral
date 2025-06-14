72e98097aea7a1008f043babbefb6f771915cd7a
Mixcoatl666 <atnoah666@gmail.com>
2025-06-13 19:44:07 -0600

    Octavo Commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,20 +4,20 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark bg-opacity-75 navbar-dark">
       <div className="container">
-        <a className="navbar-brand fw-light" href="#">NNAT</a>
+        <a className="navbar-brand fw-light" href="/">NNAT</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">Inicio</a>
+              <a className="nav-link" href="/">Inicio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Portafolio</a>
+              <a className="nav-link" href="/portfolio">Portafolio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contacto</a>
+              <a className="nav-link" href="/contacto">Contacto</a>
             </li>
           </ul>
         </div>
